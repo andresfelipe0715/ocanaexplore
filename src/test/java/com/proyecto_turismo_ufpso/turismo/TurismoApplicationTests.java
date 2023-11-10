f094bbe365247a58375828f098da3f63244d6180
andresfelipe0715 <afguaglianonip@ufpso.edu.co>
2023-11-10 00:33:26 -0500

    mi primer commit

---
+++ src/test/java/com/proyecto_turismo_ufpso/turismo/TurismoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyecto_turismo_ufpso.turismo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TurismoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
