9667c90853146492324a73d23a52181e17bca1f9
josep <jjsuescunb@ufpso.edu.co>
2023-11-17 22:21:30 -0500

    entidad modulo plan

--- src/main/java/com/proyecto_turismo_ufpso/turismo/plans/controller/PlansController.java
+++
@@ -1,4 +0,0 @@
-package com.proyecto_turismo_ufpso.turismo.plans.controller;
-
-public class PlansController {
-}
--- src/main/java/com/proyecto_turismo_ufpso/turismo/plans/dto/PlansDto.java
+++
@@ -1,4 +0,0 @@
-package com.proyecto_turismo_ufpso.turismo.plans.dto;
-
-public class PlansDto {
-}
